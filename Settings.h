dd8f575045e825ea2d53ea9ad4dc6076433be475
FrankUlbrich <50796924+FrankUlbrich@users.noreply.github.com>
2019-07-18 18:44:05 +0200

    Add files via upload
     * 18.07.2019 FUlb  Auf die Feuchte = 10 + Feuchte   addiert zur Korrektur des BME 280
     *                  BME Temp  = -1 in Settings.h
     *                  MQTT with user / password
     *                  CO Sensor CIS 811 with delay
     *                  Send to INFLUX DB 
     *                  UV light sensor in IIC
     *****************************************************************************************************************************/
    
    /*  HARDWARE (Frank Ulbrich, Germany):
    //  WeMOS D1 mini PRO 160MHz / 16MB
    //  BMP 280 (Temperatur/Barometer/Feuchte)auf IIC 0x76
    //  BME 280 (Temperatur/Barometer) auf IIC 0x76
    //  DHT22   (Temperatur/Feuchte) auf D7
    //  CCS811  (CO Gas) auf IIC 0x5A - bridge  WAK-GND - I2C port
    //  VELM6070 UV Light Sensor IIC - risk level http://www.vishay.com/docs/84310/designingveml6070.pdf
    */

--- Settings.h
+++ Settings.h
@@ -7,41 +7,59 @@
 
 ******* configuration control constant for use of Blynk and/or Thingspeak ***/
 
-const String App1 = "BLYNK";         // empty string if not applicable -> "" else "BLYNK" 
-const String App2 = "THINGSPEAK";    // empty string if not applicable -> "" else "THINGSPEAK"
+const String App1 = "BLYNK";             // empty string if not applicable -> "" else "BLYNK" 
+const String App2 = "THINGSPEAK_off";    // empty string if not applicable -> "" else "THINGSPEAK"
+const String App3 = "MQTT_off";          // empty string if not applicable -> "" else "MQTT"
+const String App4 = "INFLUX_off";        // empty string if not applicable -> "" else "INFLUX"
 
 
 /****** Blink or ThingSpeak Settings ****************************************/
 
-char auth[] = "your Blynk Auth Token"; // Blynk Auth Token
-
-char ssid[] = "your SSID";                           // WiFi Router ssid
-char pass[] = "your Password";             // WiFi Router password
-
 const char* server = "api.thingspeak.com";        // Thingspeak Write API
 const char* api_key = "your Thingspeak API key";         // API write key 
 
-/****** MQTT Settings ********************************************************/
 
-const char* mqtt_server = "192.xxx.xxx.xxx";      // MQTT Server (broker) address
+/* Blynk Internet ********************************************************************/
+//char auth[] = "your Blynk Auth Token";               // Blynk Auth Token
 
+/* Blynk FUlb ********************************************************************/
+#define BLYNK_PRINT Serial          // Comment this out to disable prints and save space
+//#include <BlynkSimpleEsp8266.h>
 
-/****** Additional Settings **************************************************/
+char auth[] = "336676f9dxxxxxxxxxxxxx";    // Wetterstation 2.31 WeMos-4
+char ssid[] = "ZugriffspunktIoT";          // WiFi Router ssid
+char pass[] = "appassword";           // WiFi Router password
+char bserver[] =  "192.168.4.23";          // IP des lokalen Blynk Servers
+/*********************************************************************************/
 
+/****** MQTT Settings ************************************************************/
+const char* mqtt_server = "192.168.4.21";     // MQTT Server (broker) address
+const char* mqtt_user = "iobroker";           // MQTT Server (broker) userid
+const char* mqtt_pass = "iopassword";           // MQTT Server (broker) password
+/*********************************************************************************/
+
+/****** INFLUX Settings **********************************************************/
+#define INFLUXDB_HOST "192.168.4.23"          // INFLUX Server (DB) address
+#define INFLUXDB_PORT 8086                    // INFLUX Server (DB) port
+#define INFLUXDB_DATABASE "testdaten"         // INFLUX Server (DB) database
+#define INFLUXDB_USER "admin"                 // INFLUX Server (DB) influx
+#define INFLUXDB_PASS "adminpassword"          // INFLUX Server (DB) password
+/*********************************************************************************/
+
+/****** Additional Settings ******************************************************/
 #define LANGUAGE 'DE'               //either 'DE' for German or 'EN' for English
 
-#define TEMP_CORR (-1)              //Manual correction of temp sensor (mine reads 1 degree too high)
-#define ELEVATION (505)             //Enter your elevation in m ASL to calculate rel pressure (ASL/QNH) at your place
-
+#define BME_TEMP_CORR (-1)          //Manual correction of temp sensor (mine reads 1 degree too high)
+#define ELEVATION (33)              //Enter your elevation in m ASL to calculate rel pressure (ASL/QNH) at your place
+#define BME_HUM_CORR (+10)          //Humidity for BME Sensor
 #define sleepTimeMin (10)           //setting of deepsleep time in minutes (default: 10)
 
-// NTP   --> Just a remark - the program needs the time only for the timestamp, so for the Zambretti forecast
-//           the timezone and the DST (Daylight Saving Time) is irrelevant. This is why I did not take care of DST 
-//           in the code. I saw a fork on Github (truckershitch) which I believe has covered this.
-
+// NTP
 #define NTP_SERVER      "ch.pool.ntp.org"
-#define TZ              1           // (utc+) TZ in hours
-#define DST_MN          60          // use 60mn for summer time in some countries
+//#define TZ              1         // (utc+) TZ in hours
+#define TZ              0           // (utc+) TZ in hours
+//#define DST_MN          60        // use 60mn for summer time in some countries
+#define DST_MN          0           // use 60mn for summer time in some countries
 
 #define TZ_SEC          ((TZ)*3600)  // don't change this
 #define DST_SEC         ((DST_MN)*60)// don't change this
@@ -61,6 +79,11 @@ virtual pin 7 Zambrettis Words
 virtual pin 8 Accuracy in percent (%)
 virtual pin 9 Tend in Words
 virtual pin 10 Dewpoint Spread
+temperature_DHT);          // virtual pin 11
+humidity_DHT);             // virtual pin 12
+value_CO2);                // virtual pin 13
+value_TVOC);               // virtual pin 14
+value_UVindex);            // virtual pin 15
 
 ThingSpeak:
 
